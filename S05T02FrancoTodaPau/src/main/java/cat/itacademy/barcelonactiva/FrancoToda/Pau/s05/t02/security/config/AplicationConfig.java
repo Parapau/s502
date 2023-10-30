e83873f7784f7dda8e0a105ca38a45f9d3e9f323
Parapau <120369026+Parapau@users.noreply.github.com>
2023-10-30 19:46:41 +0100

    Add files via upload

--- S05T02FrancoTodaPau/src/main/java/cat/itacademy/barcelonactiva/FrancoToda/Pau/s05/t02/security/config/AplicationConfig.java
+++ S05T02FrancoTodaPau/src/main/java/cat/itacademy/barcelonactiva/FrancoToda/Pau/s05/t02/security/config/AplicationConfig.java
@@ -1,6 +1,8 @@
 package cat.itacademy.barcelonactiva.FrancoToda.Pau.s05.t02.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -42,4 +44,29 @@ public class AplicationConfig {
 			
 		};
 	}
+	
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider () {
+		
+		DaoAuthenticationProvider autenticador = new DaoAuthenticationProvider();
+		autenticador.setUserDetailsService(userDetailsService());
+		autenticador.setPasswordEncoder(passwordEncoder());
+		
+		return daoAuthenticationProvider;
+	}
+	
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		
+		PasswordEncoder encoder
+		
+		
+		
+		
+		return null;
+	}
+	
+	
 }
