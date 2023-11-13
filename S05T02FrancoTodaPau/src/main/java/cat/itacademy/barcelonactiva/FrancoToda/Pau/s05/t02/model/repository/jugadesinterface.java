89429abb78c6f7fcc1759907bd2f051d8e0f9469
Parapau <120369026+Parapau@users.noreply.github.com>
2023-11-13 12:12:10 +0100

    Delete S05T02FrancoTodaPau directory

--- S05T02FrancoTodaPau/src/main/java/cat/itacademy/barcelonactiva/FrancoToda/Pau/s05/t02/model/repository/jugadesinterface.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.FrancoToda.Pau.s05.t02.model.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import cat.itacademy.barcelonactiva.FrancoToda.Pau.s05.t02.model.Jugades;
-
-public interface jugadesinterface extends MongoRepository <Jugades, String> {
-
-}
